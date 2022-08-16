9be42a757e414ce915baab85ee7758495c466d71
Aldyy231 <53711786+Aldyy123@users.noreply.github.com>
2022-08-16 19:13:02 +0700

    Creating validation golang and tidy up view

--- controllers/BooksController.go
+++ controllers/BooksController.go
@@ -4,15 +4,25 @@ import (
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"os"
 	"strings"
 
 	Model "project/models"
 
+	validator "github.com/go-playground/validator/v10"
 	"github.com/julienschmidt/httprouter"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+type books struct {
+	Name        string `validate:"required"`
+	Author      string `validate:"required"`
+	Description string `validate:"required,min=10"`
+	ImageCover  string `validate:"required"`
+}
 
 func sqliteDB() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
@@ -23,21 +33,17 @@ func sqliteDB() *gorm.DB {
 	return db
 }
 
-func uploadFile(w http.ResponseWriter, r *http.Request) string {
-	file, _, errForm := r.FormFile("cover_image")
-
-	if errForm != nil {
-		panic("Error FormFile: " + errForm.Error())
-	}
-
-	defer file.Close()
-
+func tempFile() *os.File {
 	tempFile, errTemp := ioutil.TempFile("assets/images", "cover-*.png")
 
 	if errTemp != nil {
 		panic("Error TempDir: " + errTemp.Error())
 	}
-	defer tempFile.Close()
+
+	return tempFile
+}
+
+func saveFileImage(file multipart.File, tempFile *os.File) string {
 
 	fileBytes, err := ioutil.ReadAll(file)
 
@@ -49,6 +55,11 @@ func uploadFile(w http.ResponseWriter, r *http.Request) string {
 	var fileName = strings.Replace(tempFile.Name(), "assets", "static", -1)
 	fileName = strings.Replace(fileName, "\\", "/", -1)
 	return fileName
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) (multipart.File, *multipart.FileHeader, error) {
+	file, header, errForm := r.FormFile("cover_image")
+	return file, header, errForm
 }
 
 func renderTemplateHTML(htmlTmp string, w http.ResponseWriter, data interface{}) {
@@ -84,17 +95,45 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 func Create(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	db := sqliteDB()
 
-	if r.Method == "POST" {
-		filename := uploadFile(w, r)
-		fmt.Println(filename)
+	data := map[string]interface{}{
+		"Error": nil,
+	}
 
-		book := Model.Books{
+	if r.Method == "POST" {
+
+		file, headerFile, errForm := uploadFile(w, r)
+
+		book := books{
 			Name:        r.FormValue("name"),
 			Author:      r.FormValue("author"),
 			Description: r.FormValue("description"),
-			ImageCover:  filename,
+			ImageCover:  headerFile.Filename,
 		}
 
+		validate := validator.New()
+		err := validate.Struct(book)
+		fmt.Println(err)
+		var multiErrors = []interface{}{
+			err,
+		}
+
+		if errForm != nil {
+			multiErrors = append(multiErrors, errForm.Error())
+		}
+
+		data["Error"] = multiErrors
+
+		if err != nil {
+			renderTemplateHTML("create", w, data)
+			return
+		}
+		defer file.Close()
+
+		tempFile := tempFile()
+		defer tempFile.Close()
+
+		filename := saveFileImage(file, tempFile)
+		book.ImageCover = filename
 		db.Create(&book)
 
 		http.Redirect(w, r, "/", http.StatusFound)
@@ -109,15 +148,41 @@ func Update(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	book := Model.Books{}
 	err := db.First(&book, params.ByName("id")).Error
 
+	data := map[string]interface{}{
+		"Error": nil,
+	}
 	if err != nil {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
 	if r.Method == "POST" {
+		file, _, _ := uploadFile(w, r)
+
 		book.Name = r.FormValue("name")
 		book.Description = r.FormValue("description")
 		book.Author = r.FormValue("author")
+
+		validate := validator.New()
+		err := validate.Struct(book)
+		fmt.Println(file)
+		data["Error"] = err
+
+		if err != nil {
+			renderTemplateHTML("update", w, data)
+			return
+		}
+
+		if file != nil {
+			defer file.Close()
+
+			tempFile := tempFile()
+			defer tempFile.Close()
+
+			filename := saveFileImage(file, tempFile)
+			book.ImageCover = filename
+		}
+
 		db.Save(&book)
 
 		http.Redirect(w, r, "/", http.StatusFound)
